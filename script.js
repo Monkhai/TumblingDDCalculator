86f6cffb3dbc93822fc11bd9a8a527a9dc3ddc4c
Yohai Wiener <125396366+Monkhai@users.noreply.github.com>
2023-03-03 09:04:09 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -42,19 +42,21 @@ buttonsEl.forEach((button) => {
 });
 
 deleteBtnEl.addEventListener("click", function () {
-  skillIndex--;
-  skillValuesEl[skillIndex].textContent = "";
-  skillNamesEl[skillIndex].textContent = "";
-  skillValuesEl[skillIndex].value = "0";
+  if (skillIndex > 0) {
+    skillIndex--;
+    skillValuesEl[skillIndex].textContent = "";
+    skillNamesEl[skillIndex].textContent = "";
+    skillValuesEl[skillIndex].value = "0";
 
-  let totalDifficulty = 0;
-  skillValuesEl.forEach((skillValue) => {
-    // the parseFloat converts the value of skillValue into
-    totalDifficulty += parseFloat(skillValue.value);
-  });
+    let totalDifficulty = 0;
+    skillValuesEl.forEach((skillValue) => {
+      // the parseFloat converts the value of skillValue into
+      totalDifficulty += parseFloat(skillValue.value);
+    });
 
-  // Update the total difficulty element// .toFixed is what forces calc to show the number only with 1 digit after the decimal point
-  calcEl.textContent = totalDifficulty.toFixed(1);
+    // Update the total difficulty element// .toFixed is what forces calc to show the number only with 1 digit after the decimal point
+    calcEl.textContent = totalDifficulty.toFixed(1);
+  }
 });
 
 resetBtnEl.addEventListener("click", function () {
